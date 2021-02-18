d37d0c0e64f96c943df940841f2e74d2bfde4dbc
Lennart Eichhorn <lennart@madmanfred.com>
2021-02-18 22:40:47 +0100

    Added functions for quitting the program

---
+++ src/quit.cpp
@@ -0,0 +1,25 @@
+#include "quit.hpp"
+
+[[ noreturn ]] void quit::success(){
+  exit(0);
+}
+
+[[ noreturn ]] void quit::failedToUpload(const std::string& message){
+  std::cerr << message << '\n';
+  exit(3);
+}
+
+[[ noreturn ]] void quit::noTargetSelected(const std::string& message){
+  std::cerr << message << '\n';
+  exit(4);
+}
+
+[[ noreturn ]] void quit::invalidCliUsage(const std::string& message){
+  std::cerr << message << '\n';
+  exit(64);
+}
+
+[[ noreturn ]] void quit::failedReadingFiles(const std::string& message){
+  std::cerr << message << '\n';
+  exit(65);
+}
