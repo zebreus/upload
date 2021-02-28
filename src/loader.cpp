ee40c16f7973456785e68c3a29a319d3f5363c7e
Lennart Eichhorn <lennart@madmanfred.com>
2021-02-28 15:46:30 +0100

    Implemented continue option for individual mode

--- src/loader.cpp
+++ src/loader.cpp
@@ -21,36 +21,54 @@ void Loader::loadFilesFromSettings() {
 }
 
 void Loader::loadPath(const std::filesystem::path& path) {
-  std::filesystem::file_status fileStatus = ensureFileStatus(path);
+  try {
+    std::filesystem::file_status fileStatus = ensureFileStatus(path);
 
-  switch(fileStatus.type()) {
-    case std::filesystem::file_type::directory:
-      loadDirectory(path);
-      break;
-    case std::filesystem::file_type::regular:
-      loadRegularFile(path);
-      break;
-    case std::filesystem::file_type::symlink:
-      loadSymlink(path);
-      break;
-    case std::filesystem::file_type::character:
-      loadCharacterSpecialFile(path);
-      break;
-    case std::filesystem::file_type::fifo:
-      loadFifoFile(path);
-      break;
-    case std::filesystem::file_type::block:
-      logger.log(Logger::Topic::Fatal) << "Block special files are not yet implemented. You probably wanted to specify another file." << '\n';
+    switch(fileStatus.type()) {
+      case std::filesystem::file_type::directory:
+        loadDirectory(path);
+        break;
+      case std::filesystem::file_type::regular:
+        loadRegularFile(path);
+        break;
+      case std::filesystem::file_type::symlink:
+        loadSymlink(path);
+        break;
+      case std::filesystem::file_type::character:
+        loadCharacterSpecialFile(path);
+        break;
+      case std::filesystem::file_type::fifo:
+        loadFifoFile(path);
+        break;
+      case std::filesystem::file_type::block:
+        logger.log(Logger::LoadFatal) << "Block special files are not yet implemented. You probably wanted to specify another file." << '\n';
+        if(!settings.getContinueLoading()) {
+          quit::failedReadingFiles();
+        }
+        break;
+      case std::filesystem::file_type::socket:
+        logger.log(Logger::LoadFatal) << "Socket special files are not yet implemented. You probably wanted to specify another file." << '\n';
+        if(!settings.getContinueLoading()) {
+          quit::failedReadingFiles();
+        }
+        break;
+      case std::filesystem::file_type::unknown:
+      case std::filesystem::file_type::none:
+      case std::filesystem::file_type::not_found:
+      default:
+        logger.log(Logger::LoadFatal) << "This should not have happened, as the fileStatus for this switch should be checked before" << '\n';
+        if(!settings.getContinueLoading()) {
+          quit::failedReadingFiles();
+        }
+        break;
+    }
+    return;
+  } catch(const std::runtime_error& error) {
+    logger.log(Logger::LoadFatal) << error.what() << '\n';
+    if(!settings.getContinueLoading()) {
       quit::failedReadingFiles();
-    case std::filesystem::file_type::socket:
-      logger.log(Logger::Topic::Fatal) << "Socket special files are not yet implemented. You probably wanted to specify another file." << '\n';
-      quit::failedReadingFiles();
-    case std::filesystem::file_type::unknown:
-    case std::filesystem::file_type::none:
-    case std::filesystem::file_type::not_found:
-    default:
-      logger.log(Logger::Fatal) << "This should not have happened, as the fileStatus for this switch should be checked before" << '\n';
-      quit::failedReadingFiles();
+    }
+    return;
   }
 }
 
@@ -62,37 +80,56 @@ void Loader::loadRegularFile(const std::filesystem::path& path) {
     }
     unprocessedFilesConditionVariable.notify_one();
   } else {
-    logger.log(Logger::Fatal) << "You do not have the permission to access the file " << path
-                              << " . Contact your system administrator about that, or something. You can give everyone read "
-                                 "permissions to that file with 'sudo chmod a+r "
-                              << path << "'" << '\n';
-    quit::failedReadingFiles();
+    logger.log(Logger::LoadFatal) << "You do not have the permission to access the file " << path
+                                  << " . Contact your system administrator about that, or something. You can give everyone read "
+                                     "permissions to that file with 'sudo chmod a+r "
+                                  << path << "'" << '\n';
+    if(!settings.getContinueLoading()) {
+      quit::failedReadingFiles();
+    }
   }
 }
 
 void Loader::loadSymlink(std::filesystem::path path) {
   constexpr int maxRedirects = 16;
   int redirects = 0;
-  do {
-    try {
-      if(isReadable(path)) {
-        path = std::filesystem::read_symlink(path);
-      } else {
-        logger.log(Logger::Fatal) << "Failed to upload the target of the symlink " << path
-                                  << ", because you do not have read permissions for the symlink." << '\n';
-        quit::failedReadingFiles();
+  try {
+    do {
+      try {
+        if(isReadable(path)) {
+          path = std::filesystem::read_symlink(path);
+        } else {
+          logger.log(Logger::LoadFatal) << "Failed to open the target of the symlink " << path
+                                        << ", because you do not have read permissions for the symlink." << '\n';
+          if(!settings.getContinueLoading()) {
+            quit::failedReadingFiles();
+          }
+          return;
+        }
+        redirects++;
+        if(redirects > maxRedirects) {
+          logger.log(Logger::LoadFatal) << "Stopped reading symlink path " << path << ", because there were too many redirections." << '\n';
+          if(!settings.getContinueLoading()) {
+            quit::failedReadingFiles();
+          }
+          return;
+        }
+      } catch(const std::filesystem::filesystem_error& error) {
+        logger.log(Logger::LoadFatal) << "You tried to upload the symlink " << path << ", but it cannot be followed." << '\n';
+        logger.log(Logger::Debug) << error.what() << '\n';
+        if(!settings.getContinueLoading()) {
+          quit::failedReadingFiles();
+        }
+        return;
       }
-      redirects++;
-      if(redirects > maxRedirects) {
-        logger.log(Logger::Fatal) << "Stopped reading symlink path " << path << ", because there were too many redirections." << '\n';
-        quit::failedReadingFiles();
-      }
-    } catch(const std::filesystem::filesystem_error& error) {
-      logger.log(Logger::Fatal) << "You tried to upload the symlink " << path << ", but it cannot be followed." << '\n';
-      logger.log(Logger::Debug) << error.what() << '\n';
+    } while(ensureFileStatus(path).type() == std::filesystem::file_type::symlink);
+  } catch(const std::runtime_error& error) {
+    logger.log(Logger::LoadFatal) << error.what() << '\n';
+    if(!settings.getContinueLoading()) {
       quit::failedReadingFiles();
     }
-  } while(ensureFileStatus(path).type() == std::filesystem::file_type::symlink);
+    return;
+  }
   loadPath(path);
 }
 
@@ -104,11 +141,13 @@ void Loader::loadDirectory(const std::filesystem::path& path) {
     }
     unprocessedFilesConditionVariable.notify_one();
   } else {
-    logger.log(Logger::Fatal) << "You do not have the permission to access the directory " << path
-                              << " . Contact your system administrator about that, or something. You can give everyone read "
-                                 "permissions to that file with 'sudo chmod -R a+r "
-                              << path << "'" << '\n';
-    quit::failedReadingFiles();
+    logger.log(Logger::LoadFatal) << "You do not have the permission to access the directory " << path
+                                  << " . Contact your system administrator about that, or something. You can give everyone read "
+                                     "permissions to that file with 'sudo chmod -R a+r "
+                                  << path << "'" << '\n';
+    if(!settings.getContinueLoading()) {
+      quit::failedReadingFiles();
+    }
   }
 }
 
@@ -116,9 +155,11 @@ void Loader::loadFifoFile(const std::filesystem::path& path) {
   if(isReadable(path)) {
     startStreamThread(path);
   } else {
-    logger.log(Logger::Fatal) << "You do not have the permission to access the fifo file " << path
-                              << " . Contact your system administrator about that, or something." << '\n';
-    quit::failedReadingFiles();
+    logger.log(Logger::LoadFatal) << "You do not have the permission to access the fifo file " << path
+                                  << " . Contact your system administrator about that, or something." << '\n';
+    if(!settings.getContinueLoading()) {
+      quit::failedReadingFiles();
+    }
   }
 }
 
@@ -126,9 +167,11 @@ void Loader::loadCharacterSpecialFile(const std::filesystem::path& path) {
   if(isReadable(path)) {
     startStreamThread(path);
   } else {
-    logger.log(Logger::Fatal) << "You do not have the permission to access the character special file " << path
-                              << " . Contact your system administrator about that, or something." << '\n';
-    quit::failedReadingFiles();
+    logger.log(Logger::LoadFatal) << "You do not have the permission to access the character special file " << path
+                                  << " . Contact your system administrator about that, or something." << '\n';
+    if(!settings.getContinueLoading()) {
+      quit::failedReadingFiles();
+    }
   }
 }
 
@@ -155,8 +198,11 @@ void Loader::startStreamThread(const std::filesystem::path& path) {
           loadPath(std::string(buffer));
         }
       } else if(!stream->eof()) {
-        logger.log(Logger::Fatal) << "Failed to read file from stream" << '\n';
-        quit::failedReadingFiles();
+        if(buffer[0] != 0) {
+          logger.log(Logger::Info) << "Filename received from stream is longer than 512 characters. This is currently not supported." << '\n';
+          while(stream->good() && stream->get() != '\n') {
+          }
+        }
       }
     }
     if(stream != &std::cin) {
@@ -174,17 +220,17 @@ void Loader::startStreamThread(const std::filesystem::path& path) {
 std::filesystem::file_status Loader::ensureFileStatus(const std::filesystem::path& path) {
   try {
     std::filesystem::file_status fileStatus = std::filesystem::status(path);
+    std::stringstream message;
     switch(fileStatus.type()) {
       case std::filesystem::file_type::none:
-        logger.log(Logger::Fatal) << "Failed to get information about the file " << path << ". " << '\n';
-        quit::failedReadingFiles();
+        message << "Failed to get information about the file " << path << ". ";
+        throw std::runtime_error(message.str());
       case std::filesystem::file_type::not_found:
-        logger.log(Logger::Fatal) << path << " does not exist. Maybe check for typos." << '\n';
-        quit::failedReadingFiles();
+        message << path << " does not exist. Maybe check for typos.";
+        throw std::runtime_error(message.str());
       case std::filesystem::file_type::unknown:
-        logger.log(Logger::Fatal) << "Failed to determine to filetype of " << path
-                                  << " . You should check, that you have permissions to access that file. " << '\n';
-        quit::failedReadingFiles();
+        message << "Failed to determine to filetype of " << path << " . You should check, that you have permissions to access that file. ";
+        throw std::runtime_error(message.str());
       case std::filesystem::file_type::symlink:
       case std::filesystem::file_type::block:
       case std::filesystem::file_type::character:
@@ -196,9 +242,10 @@ std::filesystem::file_status Loader::ensureFileStatus(const std::filesystem::pat
         return fileStatus;
     }
   } catch(const std::filesystem::filesystem_error& error) {
-    logger.log(Logger::Fatal) << "Failed to get information about the file " << path << ". Maybe the path is not formatted correctly?" << '\n';
+    std::stringstream message;
+    message << "Failed to get information about the file " << path << ". Maybe the path is not formatted correctly?" << '\n';
     logger.log(Logger::Debug) << error.what() << '\n';
-    quit::failedReadingFiles();
+    throw std::runtime_error(message.str());
   }
 }
 
@@ -231,27 +278,32 @@ std::shared_ptr<File> Loader::createArchive(const std::vector<std::filesystem::p
 
       for(auto& p : std::filesystem::recursive_directory_iterator(canonicalPath)) {
         // Absolute path in filesystem
-        std::filesystem::file_status status = ensureFileStatus(p.path());
-        if(status.type() != std::filesystem::file_type::regular && status.type() != std::filesystem::file_type::directory) {
-          logger.log(Logger::Fatal) << "Only regular files and directories can be archived. You tried to archive " << path
-                                    << ", which is neither." << '\n';
-          quit::failedReadingFiles();
-        }
-        std::filesystem::path realPath = std::filesystem::canonical(p.path(), error);
-        if(error) {
-          logger.log(Logger::Fatal) << "Failed to create canonical path of " << path << " . This should not happen." << '\n';
-          quit::failedReadingFiles();
-        }
+        try {
+          std::filesystem::file_status status = ensureFileStatus(p.path());
+          if(status.type() != std::filesystem::file_type::regular && status.type() != std::filesystem::file_type::directory) {
+            logger.log(Logger::Fatal) << "Only regular files and directories can be archived. You tried to archive " << path
+                                      << ", which is neither." << '\n';
+            quit::failedReadingFiles();
+          }
+          std::filesystem::path realPath = std::filesystem::canonical(p.path(), error);
+          if(error) {
+            logger.log(Logger::Fatal) << "Failed to create canonical path of " << path << " . This should not happen." << '\n';
+            quit::failedReadingFiles();
+          }
 
-        // Path in archive
-        std::filesystem::path resultPath = std::filesystem::relative(realPath, basePath);
+          // Path in archive
+          std::filesystem::path resultPath = std::filesystem::relative(realPath, basePath);
 
-        if(std::filesystem::is_directory(realPath)) {
-          std::string resultDirPath = resultPath.string() + "/";
-          file.writestr(resultDirPath, "");
-        } else {
-          File f(realPath);
-          file.writestr(resultPath, f.getContent());
+          if(std::filesystem::is_directory(realPath)) {
+            std::string resultDirPath = resultPath.string() + "/";
+            file.writestr(resultDirPath, "");
+          } else {
+            File f(realPath);
+            file.writestr(resultPath, f.getContent());
+          }
+        } catch(const std::runtime_error& error) {
+          logger.log(Logger::Fatal) << error.what() << '\n';
+          quit::failedReadingFiles();
         }
       }
 
@@ -300,7 +352,7 @@ std::shared_ptr<File> Loader::getNextFile() {
           return std::make_shared<File>(path);
         }
       } catch(const std::runtime_error& error) {
-        logger.log(Logger::Topic::Debug) << "All files loaded.";
+        logger.log(Logger::Debug) << "All files loaded.";
         return std::shared_ptr<File>(nullptr);
       }
     }
